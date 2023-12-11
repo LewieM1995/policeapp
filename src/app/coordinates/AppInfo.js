296e224f29ded7f46cfb14532a5de648c8cb2b07
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2023-12-11 09:15:17 +0000

    Update AppInfo.js

--- src/app/coordinates/AppInfo.js
+++ src/app/coordinates/AppInfo.js
@@ -2,9 +2,24 @@
 export default function AppInfo() {
   return (
     <>
-      <h2 style={{textAlign: 'center'}}>Use Latitude and Longitude pairs to search the api and return some stats/figures, or selected a date and one of the present location from the dropdown</h2>
-      <p style={{textAlign: 'center'}}>Due to api constraints some dates will be slow to load and 2 pairs will not work.</p>
-      <p style={{textAlign: 'center', fontSize: '25px'}}>A couple tools to help with latitude and longitude <a href='https://www.keene.edu/campus/maps/tool/' target="_blank">One</a> <a href='https://gps-coordinates.org'  target='_blank'>Two</a> *Disclaimer - I am not associated with these sites in any way*</p>
+      <h2 style={{ textAlign: 'center' }}>
+        Explore API Stats with Latitude and Longitude Pairs or Choose a Date and Location
+      </h2>
+      <p style={{ textAlign: 'center' }}>
+        Please note that due to API constraints, some dates may have slower loading times,
+        and using two pairs simultaneously might not be supported.
+      </p>
+      <p style={{ textAlign: 'center', fontSize: '1.5rem' }}>
+        For assistance with latitude and longitude, consider using these helpful tools:
+        {' '}
+        <a href='https://www.keene.edu/campus/maps/tool/' target="_blank" rel="noopener noreferrer">Tool One</a>
+        {' '}
+        and
+        {' '}
+        <a href='https://gps-coordinates.org' target='_blank' rel="noopener noreferrer">Tool Two</a>.
+        {' '}
+        <em>(Disclaimer: I am not associated with these sites in any way.)</em>
+      </p>
     </>
   )
 }
