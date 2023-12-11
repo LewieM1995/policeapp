77842fecdeecf2916cc2543d90bda6075fc73d53
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2023-12-11 08:00:49 +0000

    changes to input type

--- src/app/coordinates/CoordinateInput.js
+++ src/app/coordinates/CoordinateInput.js
@@ -7,10 +7,10 @@ export default function CoordinateInput ({ label1, value1, onChange1, placeholde
     <>
     <div className="co-ords-container">
         <label>{label1}:</label>
-        <input type="text" inputMode='numeric' pattern='[0-9.]' value={value1} onChange={onChange1} placeholder={placeholder1} />
+        <input type="number" step="any" inputMode='numeric' value={value1} onChange={onChange1} placeholder={placeholder1} />
         <label>{label2}:</label>
-        <input type="text" inputMode='numeric' pattern='[0-9.]' value={value2} onChange={onChange2} placeholder={placeholder2} />
+        <input type="number" step="any" inputMode='numeric' value={value2} onChange={onChange2} placeholder={placeholder2} />
     </div>
   </>
- )  
+ )   
 };
