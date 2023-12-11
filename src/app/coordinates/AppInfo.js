31e668c478b713fb7bcad3cade996460b46b6525
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2023-12-11 07:52:37 +0000

    Update AppInfo.js

--- src/app/coordinates/AppInfo.js
+++ src/app/coordinates/AppInfo.js
@@ -2,10 +2,9 @@
 export default function AppInfo() {
   return (
     <>
-      <h1 style={{textAlign: 'center'}}>Stop & Search using Latitude & Longitude</h1>
+      <h2 style={{textAlign: 'center'}}>Use Latitude and Longitude pairs to search the api and return some stats/figures, or selected a date and one of the present location from the dropdown</h2>
       <p style={{textAlign: 'center'}}>Due to api constraints some dates will be slow to load and 2 pairs will not work.</p>
-      <p style={{textAlign: 'center', fontSize: '25px'}}>Use these tools to search using coordinates<a href='https://www.keene.edu/campus/maps/tool/' target="_blank">One</a> helpful.
-      <a href='https://gps-coordinates.org'  target='_blank'>Two</a></p>
+      <p style={{textAlign: 'center', fontSize: '25px'}}>A couple tools to help with latitude and longitude <a href='https://www.keene.edu/campus/maps/tool/' target="_blank">One</a> <a href='https://gps-coordinates.org'  target='_blank'>Two</a> *Disclaimer - I am not associated with these sites in any way*</p>
     </>
   )
 }
