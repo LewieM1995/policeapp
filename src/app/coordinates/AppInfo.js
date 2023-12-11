130fc66c4a36bd5120144ecfb40a9dd9e300a351
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2023-12-11 07:43:48 +0000

    Update AppInfo.js

--- src/app/coordinates/AppInfo.js
+++ src/app/coordinates/AppInfo.js
@@ -4,8 +4,8 @@ export default function AppInfo() {
     <>
       <h1 style={{textAlign: 'center'}}>Stop & Search using Latitude & Longitude</h1>
       <p style={{textAlign: 'center'}}>Due to api constraints some dates will be slow to load and 2 pairs will not work.</p>
-      <p style={{textAlign: 'center', fontSize: '25px', padding: '10px'}}>Default date is 2023-05.</p>
-      <p style={{textAlign: 'center', fontSize: '25px'}}>You might find <a href='https://www.keene.edu/campus/maps/tool/' target="_blank">this</a> helpful.</p>
+      <p style={{textAlign: 'center', fontSize: '25px'}}>Use these tools to search using coordinates<a href='https://www.keene.edu/campus/maps/tool/' target="_blank">One</a> helpful.
+      <a href='https://gps-coordinates.org'  target='_blank'>Two</a></p>
     </>
   )
 }
