0d90ee447d365a9b367cc89b070d824a34e0f0fb
LewieM1995 <113435933+LewieM1995@users.noreply.github.com>
2023-12-11 09:31:33 +0000

    Update page.js

--- src/app/byforce/page.js
+++ src/app/byforce/page.js
@@ -10,7 +10,7 @@ import Output from "./components/OutputForForceSearch";
 export default function Byforce() {
 
   const [dropdown, setDropDown] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState("01-2023");
   const [listData, setListData] = useState([]);
   const [data, setData] = useState([]);
   
